cf9cdee2bdf7c93d5bb51eef22f6d7804b07931d
Codi Huston (Home) <codi.huston540@gmail.com>
2020-11-12 22:01:53 -0600

    fix: forgot to update some env names
    - renamed env var prefix to GMH in test file

--- main_test.go
+++ main_test.go
@@ -18,9 +18,9 @@ var a App
 
 func TestMain(m *testing.M) {
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		os.Getenv("GMH_DB_USERNAME"),
+		os.Getenv("GMH_DB_PASSWORD"),
+		os.Getenv("GMH_DB_NAME"))
 
 	ensureTableExists()
 	code := m.Run()
