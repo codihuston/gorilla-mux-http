f7156b103c41456c6bf2e78a44ca9cd6a9b71864
Codi Huston (Home) <codi.huston540@gmail.com>
2020-11-12 21:06:12 -0600

    docs: add readme

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ func main() {
 	a := App{}
 
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"),
+		os.Getenv("GMH_DB_USERNAME"),
+		os.Getenv("GMH_DB_PASSWORD"),
+		os.Getenv("GMH_DB_NAME"),
 	)
 
 	a.Run(":8010")
